5930d18db08104d1e67b46b8f49766f734c9493d
Judith Liem <25380136+jjliem@users.noreply.github.com>
2021-09-14 10:46:33 -0500

    add material ui and mediacard

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import MediaCard from "./MediaCard";
 
 export default function PlantList() {
   return (
     <div>
       <h2>Plant List</h2>
+      <MediaCard></MediaCard>
+      <MediaCard></MediaCard>
+      <MediaCard></MediaCard>
+      <MediaCard></MediaCard>
     </div>
   );
 }
